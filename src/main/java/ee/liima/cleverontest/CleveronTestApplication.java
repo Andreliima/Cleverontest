23b23ec78c4fe9ebe3744dac6c1744a5316a8938
Andre Liima <andreliima147@gmail.com>
2021-01-06 20:33:13 +0200

    init

---
+++ src/main/java/ee/liima/cleverontest/CleveronTestApplication.java
@@ -0,0 +1,13 @@
+package ee.liima.cleverontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleveronTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleveronTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/liima/cleverontest/CleveronTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.liima.cleverontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleveronTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
